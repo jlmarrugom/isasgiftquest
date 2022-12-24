64403aa418cc871a22eda4a652ea92880d80e7e3
José Luis Marrugo Madrid <61395538+jlmarrugom@users.noreply.github.com>
2022-12-24 16:06:52 -0500

    second_version

--- main.py
+++ main.py
@@ -7,15 +7,4 @@ points = 0
 
 st.write("### Responde a las preguntas y podrás ganar fabulosos premios! 🎁")
 
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
-
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-displayPDF("attachments/k50valid until  22022023.pdf")
+st.image("attachments/ipvalid until  22022023.png")
